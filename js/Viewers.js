4b3422a716cae4a74202acd7133c7559f1fbe2ab
dlunacsdocs <dluna@cs-docs.com>
2015-11-10 21:47:49 -0600

    Implementado Visor de imagenes

--- js/Viewers.js
+++ js/Viewers.js
@@ -20,6 +20,7 @@
 function Preview(tipo, IdGlobal, IdFile,  Source)
 {    
     $('#div_vista_previa').remove();   
+    
     switch (Source)
     {
         case 'Content':
@@ -33,13 +34,18 @@ function Preview(tipo, IdGlobal, IdFile,  Source)
             DocEnvironment.GetProperties();
             break;
     }
-
-    if(tipo=='jpg' || tipo=='png' || tipo=='tiff' || tipo=='JPG' || tipo=='PNG' || tipo=='TIFF' || tipo=='jpeg' || tipo=='JPEG')
+    
+    tipo = String(tipo.toLowerCase());
+    
+    if(tipo ==='jpg' || tipo==='png' || tipo==='tiff' || tipo==='jpge')
     {      
-        var RutaArchivoServer=location.host+'/'+Ruta;
+        var RutaArchivoServer=location.host+'/'+DocEnvironment.FileRoute;
+        
         $('#VistaPrevia').append('<div id="div_vista_previa" class="viewer"></div>');
+        
         $('#div_vista_previa').dialog({width:Wvisor, height:Hvisor, minWidth:minWvisor, minHeight:minHvisor, title:"Vista Previa", modal:true,closeOnEscape:false}).dialogExtend(BotonesWindow);
-       var iv2 = $("#div_vista_previa").iviewer(
+        
+        var iv2 = $("#div_vista_previa").iviewer(
                   {
                       src: RutaArchivoServer
                   });
@@ -58,22 +64,28 @@ function Preview(tipo, IdGlobal, IdFile,  Source)
                     return false;
                   });
     }
-    else 
-    {
-        if(tipo==='pdf' || tipo==='PDF')
-        {            
-            
-            PDFJS.workerSrc = 'apis/pdf.js-master/src/worker_loader.js';
-            ArrayNotes=new Array();
-            PaginaActual=0;
-            ArrayObteinNotes=0;
-//            alert(DocumentEnvironment+DocumentEnvironment.FileRoute+tipo+IdGlobal+IdFile+Source);
-            DEFAULT_URL=DocEnvironment.FileRoute;
-            PDFView.open(DocEnvironment.FileRoute, 0);                           
-            if($.type(DocEnvironment)==='object');
-                Notes = new ClassNotes(DocEnvironment.IdRepository, DocEnvironment.RepositoryName, DocEnvironment.IdFile, DocEnvironment.FileName, DocEnvironment.IdGlobal);
-          $('#DivPdfViewer').dialog({width:Wvisor, height:Hvisor, minWidth:minWvisor, minHeight:minHvisor, title:"Vista Previa", modal:true}).dialogExtend(BotonesWindow);                                               
-        }
-        
-    }                
+    
+    $('#div_vista_previa').append('<div class = "iviewer_common iviewer_button"><img src = "../img/note.png"></div>');
+    
+    if(tipo==='pdf')
+        pdfViewer(DocEnvironment);
+    
 }
+
+
+
+function pdfViewer(DocEnvironment){
+     PDFJS.workerSrc = 'apis/pdf.js-master/src/worker_loader.js';
+    ArrayNotes=new Array();
+    PaginaActual=0;
+    ArrayObteinNotes=0;
+//            alert(DocumentEnvironment+DocumentEnvironment.FileRoute+tipo+IdGlobal+IdFile+Source);
+    DEFAULT_URL = DocEnvironment.FileRoute;
+    PDFView.open(DocEnvironment.FileRoute, 0);     
+
+    if($.type(DocEnvironment)==='object');
+        Notes = new ClassNotes(DocEnvironment.IdRepository, DocEnvironment.RepositoryName, DocEnvironment.IdFile, DocEnvironment.FileName, DocEnvironment.IdGlobal);
+
+    $('#DivPdfViewer').dialog({width:Wvisor, height:Hvisor, minWidth:minWvisor, minHeight:minHvisor, title:"Vista Previa", modal:true}).dialogExtend(BotonesWindow);
+}
+
